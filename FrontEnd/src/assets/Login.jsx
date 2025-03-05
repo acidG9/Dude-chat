aab69e4d67730b9d203696dc5e1d4b4cf765d940
Akshansh Vaishnav <akshanshvaishnavpaypal@gmail.com>
2025-03-05 14:59:43 +0530

    bcrypt and login page updated

--- FrontEnd/src/assets/Login.jsx
+++ FrontEnd/src/assets/Login.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
+    const [isRegistered, setIsRegistered]=React.useState(true);
     const navigate= useNavigate();
+
+    function toggle(){
+        setIsRegistered(!isRegistered);
+    }
     
     return (
         <div className="login-container">
@@ -13,14 +19,16 @@ const Login = () => {
             </div>
             <div className="login-form">
                 <div>
-                    <h1>Login to your Account</h1>
+                    {isRegistered?(<h1>Login to your Account</h1>):(<h1>Register your Account</h1>)}
                     <TextField id="outlined-basic" className='login-input' label="UserName" variant="outlined" />
+                    {isRegistered||(<TextField id="outlined-basic" className='login-input' label="Email" variant="outlined" />)}
                     <TextField id="outlined-basic" className='login-input' label="Password" variant="outlined" />
                     <Button variant="outlined" color="success" onClick={
                         ()=>{
                             navigate('app/welcome')
                         }
-                    }>Login</Button>
+                    }>{isRegistered?(<p>Login</p>):(<p>Register</p>)}</Button>
+                    {isRegistered?(<p>Don&apos;t have an account...    <span onClick={toggle} >register</span></p>):(<p>Already have an account...    <span onClick={toggle} >login</span></p>)}
                 </div>
             </div>
         </div>
